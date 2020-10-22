c683e4151af78d1e5a7c62b839e3ffb15cca10f4
sidney.lira <sidney.lira@murah.com.br>
2020-10-22 09:34:26 -0300

    codigo inicial git status!

---
+++ rest-api-java/src/main/java/br/com/liraquality/LQ_Startup.java
@@ -0,0 +1,13 @@
+package br.com.liraquality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LQ_Startup {
+	public static void main(String[] args) {
+		SpringApplication.run(LQ_Startup.class, args);
+	}
+
+
+}
